74188e51ad8b0142a3a5b94b445374a51f15d203
gaoc <sfme@qq.com>
2015-06-08 01:19:02 +0800

    加入Talker类 删除或者排除部分无用的类和项目
    该类作为主要的功能提供者，提供serialport的构造参数，以及数据到达时候的事件处理函数

---
+++ Windup.SerialTalker/Talker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Windup.SerialTalker
+{
+	public class Talker
+	{
+		public Talker ()
+		{
+		}
+
+		public Talker(string serialName, int baudRate, LineBreak lineBreak)
+		{
+		}
+
+		public Talker(AgentSetting setting, LineBreak lineBreak)
+		{
+		}
+	}
+}
+
