014532a44a2e5ddea3f9d5eb8c77db6892381005
gaoc <sfme@qq.com>
2015-06-11 02:22:39 +0800

    keep code simple

---
+++ Windup.Main/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Windup.SerialTalker;
+
+namespace Windup.Main
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var talker = new Talker () {
+				PortName = "COM3",
+				BaudRate = 9600,
+				LineBreak = new LineBreak(){
+					Type = "linux"
+				},
+				Proc = list => {
+					foreach (var i in list) {
+						Console.WriteLine (i);
+					}
+				}
+			};
+
+			for (int i = 0; i < 1000; i++) {
+				talker.Write (new byte[] { 10, 12, 11, 17, 18 });
+			}
+
+			Console.Read ();
+		}
+	}
+}
--- Windup.SerialTalker/Talker.cs
+++ Windup.SerialTalker/Talker.cs
@@ -8,38 +8,96 @@ namespace Windup.SerialTalker
 	public class Talker
 	{
 		//处理到达数据的回调函数，Talker的最核心的处理
-		public delegate void ByteListProc(List<byte> bytes);
-		public ByteListProc byteListProc;
+		public delegate void ByteListProc (List<byte> bytes);
+
+		ByteListProc _byteListProc;
+
+		SerialAgent _sa;
+		Analyzer _analyzer;
 
 		public Talker ()
 		{
+			LineBreak = new LineBreak () {
+				Type = "linux"
+			};
+
+			_sa = new SerialAgent ("INIT");
+			_analyzer = new Analyzer ();
+			_analyzer.SetLineBreak (LineBreak);
+			_analyzer.DataListReadyEvent += OnDataListReady;
 		}
 
-		public Talker(string portName, int baudRate, LineBreak lineBreak)
-		{
-			var sa = new SerialAgent (portName, baudRate);
-			CreateAnalyzer (sa, lineBreak);
+		public string PortName {
+			get { 
+				return _sa.AgentPortName;
+			}
+			set { 
+				_sa.AgentPortName = value;
+			}
 		}
 
-		public Talker(SPSetting setting, LineBreak lineBreak)
-		{
-			var sa = CreateAgent(setting);
-			CreateAnalyzer (sa, lineBreak);
+		public int BaudRate {
+			get {
+				return _sa.AgentBaudRate;
+			}
+			set {
+				_sa.AgentBaudRate = value;
+				_analyzer.SetAgent (_sa);
+			}
+		}
+
+		public Parity Parity {
+			get {
+				return _sa.AgentParity;
+			}
+			set {
+				_sa.AgentParity = value;
+				_analyzer.SetAgent (_sa);
+			}
+		}
+
+		public int DataBits {
+			get {
+				return _sa.AgentDataBits;
+			}
+			set {
+				_sa.AgentDataBits = value;
+				_analyzer.SetAgent (_sa);
+			}
+		}
+
+		public StopBits StopBits {
+			get {
+				return _sa.AgentStopBits;
+			}
+			set {
+				_sa.AgentStopBits = value;
+				_analyzer.SetAgent (_sa);
+			}
+		}
+
+		public ByteListProc Proc {
+			get {
+				return _byteListProc;
+			}
+			set { 
+				_byteListProc += value;
+			}
+		}
+
+		public LineBreak LineBreak {
+			get;
+			set;
 		}
 
 		/// <summary>
-		/// Creates the analyzer.
+		/// Write the specified what.
 		/// </summary>
-		/// <returns>The analyzer.</returns>
-		/// <param name="sa">Sa.</param>
-		/// <param name="lineBreak">Line break.</param>
-		Analyzer CreateAnalyzer(SerialAgent sa, LineBreak lineBreak)
+		/// <param name="what">What.</param>
+		public bool Write (byte[] what)
 		{
-			var analyzer = new Analyzer ();
-			analyzer.SetAgent (sa);
-			analyzer.SetLineBreak (lineBreak);
-			analyzer.DataListReadyEvent += OnDataListReady;
-			return analyzer;
+			var status = _sa.AgentWrite (what);
+			return 0 == status ? true : false;
 		}
 
 		/// <summary>
@@ -47,61 +105,21 @@ namespace Windup.SerialTalker
 		/// </summary>
 		/// <param name="o">O.</param>
 		/// <param name="e">E.</param>
-		void OnDataListReady(object o, DataListReadyEventArgs e)
+		void OnDataListReady (object o, DataListReadyEventArgs e)
 		{
-			byteListProc (e.Bytes);
-		}
-
-		/// <summary>
-		/// Creates the agent.
-		/// </summary>
-		/// <returns>The agent.</returns>
-		/// <param name="setting">Setting.</param>
-		SerialAgent CreateAgent(SPSetting setting)
-		{
-			var sa = new SerialAgent() {
-				AgentPortName = setting.PortName,
-				AgentBaudRate = setting.BaudRate,
-				AgentParity = setting.Parity,
-				AgentDataBits = setting.DataBits,
-				AgentStopBits = setting.StopBits
-			};
-			return sa;
-		}
-
-		/// <summary>
-		/// Creates the agents.
-		/// </summary>
-		/// <returns>The agents.</returns>
-		/// <param name="settings">Settings.</param>
-		IEnumerable<SerialAgent> CreateAgents(IEnumerable<SPSetting> settings)
-		{
-			var list = new List<SerialAgent>();
-			foreach (var sa in settings.Select(setting => new SerialAgent(
-				portName: setting.PortName,
-				baudRate: setting.BaudRate,
-				parity: setting.Parity,
-				dataBits: setting.DataBits,
-				stopBits: setting.StopBits
-			))) {
-				list.Add(sa);
-			}
-			return list;
+			_byteListProc (e.Bytes);
 		}
 
 		/// <summary>
 		/// Gets the serial ports.
 		/// </summary>
 		/// <returns>The serial ports.</returns>
-		public string[] GetSerialPorts()
+		public string[] GetSerialPorts ()
 		{
 			string[] allSerial = null;
-			try
-			{
-				allSerial = SerialPort.GetPortNames();
-			}
-			catch /*(Exception e)*/
-			{
+			try {
+				allSerial = SerialPort.GetPortNames ();
+			} catch { /*(Exception e)*/
 				//throw new Exception(e.Message);
 				return null;
 			}
@@ -109,4 +127,3 @@ namespace Windup.SerialTalker
 		}
 	}
 }
-
