5703e99df3de41b9a8e60334168b02c1241093a9
gaoc <sfme@qq.com>
2015-06-10 12:13:32 +0800

    add comments

--- Windup.SerialTalker/Talker.cs
+++ Windup.SerialTalker/Talker.cs
@@ -5,8 +5,10 @@ namespace Windup.SerialTalker
 {
 	public class Talker
 	{
+		//处理到达数据的回调函数，Talker的最核心的处理
 		public delegate void ByteListProc(List<byte> bytes);
 		public ByteListProc byteListProc;
+
 		public Talker ()
 		{
 		}
@@ -23,6 +25,12 @@ namespace Windup.SerialTalker
 			CreateAnalyzer (sa, lineBreak);
 		}
 
+		/// <summary>
+		/// Creates the analyzer.
+		/// </summary>
+		/// <returns>The analyzer.</returns>
+		/// <param name="sa">Sa.</param>
+		/// <param name="lineBreak">Line break.</param>
 		Analyzer CreateAnalyzer(SerialAgent sa, LineBreak lineBreak)
 		{
 			var analyzer = new Analyzer ();
@@ -32,6 +40,11 @@ namespace Windup.SerialTalker
 			return analyzer;
 		}
 
+		/// <summary>
+		/// Analyzer中的串口返回的字节列表到达时的数据处理函数.
+		/// </summary>
+		/// <param name="o">O.</param>
+		/// <param name="e">E.</param>
 		void OnDataListReady(object o, DataListReadyEventArgs e)
 		{
 			byteListProc (e.Bytes);
