d40c6221ae133cdc451992838b1b67f68b8064aa
gaoc <sfme@qq.com>
2015-06-12 02:09:46 +0800

    new method to create a instance of talker, add comments

--- Windup.Main/Program.cs
+++ Windup.Main/Program.cs
@@ -7,24 +7,36 @@ namespace Windup.Main
 	{
 		public static void Main (string[] args)
 		{
-			var talker = new Talker () {
-				PortName = "COM3",
-				BaudRate = 9600,
-				LineBreak = new LineBreak(){
-					Type = "linux"
-				},
-				Proc = list => {
-					foreach (var i in list) {
-						Console.WriteLine (i);
+			//test SerialPort
+			if (Talker.TouchSerialPort ("COM3", 9600)) {
+				//create a instance of Talker.
+				var talker = new Talker () {
+					PortName = "COM3",
+					BaudRate = 9600,
+					LineBreak = new LineBreak () { //set linebreak type
+						Type = "nt",
+					},
+					Proc = list => {
+						var str = string.Empty;
+						foreach (var i in list) {
+							var ch = (char)i;
+							str += ch;
+						}
+						Console.WriteLine ("echo str: {0}", str);
 					}
+				};
+
+				//open talker if it's not opened.
+				if (!talker.IsOpen) {
+					talker.Open (); 
 				}
-			};
 
-			for (int i = 0; i < 1000; i++) {
-				talker.Write (new byte[] { 10, 12, 11, 17, 18 });
+				//write data to serialport.
+				for (int i = 0; i < 1000; i++) {
+					talker.Write (new byte[] { 10, 12, 11, 17, 18 });
+				}
+				Console.Read ();
 			}
-
-			Console.Read ();
 		}
 	}
 }
